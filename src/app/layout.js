5eda61f6b8f5395b935d3dacb8a5883ee6989ad1
abc <abc@example.com>
2025-07-15 07:49:07 -0700

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 import { Geist, Geist_Mono } from "next/font/google";
 
 
@@ -25,6 +27,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
